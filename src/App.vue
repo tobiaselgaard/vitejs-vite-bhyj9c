6352190abced7e0a13a6fbf89c284c5264aa762e
tobiaselgaard <161316401+tobiaselgaard@users.noreply.github.com>
2024-09-18 13:22:22 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+function showRadialMenu(event) {
+  let cursorPositionX = event.x;
+  let cursorPositionY = event.y;
+
+  let firstButtonLeftProperty = cursorPositionX;
+  let firstButtonTopProperty = cursorPositionY;
+}
+</script>
+
+<template @click="showRadialMenu" (event)></template>
